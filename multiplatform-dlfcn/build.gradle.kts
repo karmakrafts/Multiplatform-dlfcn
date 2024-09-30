1045c2fa306ddb39e5594672346262a0d902f4fd
KitsuneAlex <alexh@karmakrafts.dev>
2024-09-30 18:52:09 +0200

    Use absolute path again

--- multiplatform-dlfcn/build.gradle.kts
+++ multiplatform-dlfcn/build.gradle.kts
@@ -47,7 +47,7 @@ kotlin {
                 linkerOpts(
                     "-L${dlfcnHome / "lib"}",
                     "-lssp", // Stack protector support for canaries
-                    "-ldl"
+                    "-l${dlfcnHome / "lib" / "libdl.a"}"
                 )
             }
         }
